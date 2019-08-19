1721efe17028d271181301bbfd427b0216bba479
Jordan <jordandoan@hotmail.com>
2019-08-19 15:47:38 -0700

    Added dynamic routing for navbar links and dynamic rendering of secondary navbar

--- apple-nav/src/components/Nav/SideBar.js
+++ apple-nav/src/components/Nav/SideBar.js
@@ -2,10 +2,17 @@ import React from "react";
 import Data from "./Data";
 
 const SideBar = (props) => {
-    let category = Data.find(props.id);
-    return ( 
-        <div> I am a sidebar! </div>
-    );
-}
+    let category = Data.find(cat => cat.id == props.id);
+    return (
+        <div className="secondary-nav-bar">
+            {category.sidebar.map((product) => 
+                <div key={product.text} className="product">
+                    <img src={product.img}/>
+                    <p>{product.text}</p>
+                </div>
+            )}
+        </div>
+    )
+};
 
 export default SideBar;
